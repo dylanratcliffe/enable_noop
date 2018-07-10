7697fa6585dbe928e39c5452fff91be5c3889ed1
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2018-07-10 16:50:10 +1000

    Fixed object

--- tasks/init.rb
+++ tasks/init.rb
@@ -7,7 +7,7 @@ begin
   @configfile = Puppet.settings['config']
   x = ::Puppet::Resource.new('ini_setting', 'noop', parameters: { ensure: 'present', path: @configfile, section: 'agent', setting: 'noop', value: 'true' })
   _result, _report = ::Puppet::Resource.indirection.save(x)
-  Puppet.reparse_config_files
+  Puppet.settings.reparse_config_files
   puts "Noop is now #{Puppet.settings['noop']}"
 rescue => e
   raise "Could not manage noop: #{e}"
